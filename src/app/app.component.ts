cf10209c0665052ee9209504243c5e507a12cefe
BlagojeV93 <blagoje.vukovic@hotmail.com>
2018-02-27 23:31:46 +0100

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserServiceService } from './user-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+  user1Activated = false;
+  user2Activated = false;
+  Roda: Subscription;
+
+  constructor(private usersService: UserServiceService) {}
+
+  ngOnInit(){
+    this.Roda = this.usersService.UserActivated.subscribe(
+      (id: number) => {
+        if (id === 1) {
+          this.user1Activated = true;
+        } else if (id === 2) {
+          this.user2Activated = true;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.Roda.unsubscribe();
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
+import { Observer, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
-  ngOnInit() {
-    // const MyNumbers = Observable.interval(1000).subscribe(
-    //   (number: number) => {
-    //     console.log(number);
-    //   }
-    // )
+  obsBrojevi : Subscription;
+  obsPaketici: Subscription;
 
+  ngOnInit() {
+    const MyNumbers = Observable.interval(1000).map(
+      (data: number) => {
+       return data - 0.5;
+      }
+    );
+
+     this.obsBrojevi = MyNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    )
+
+    const MyObservable = Observable.create( (observer: Observer<String>) => {
+        setTimeout(() => {
+          observer.next('Prvi paketic');
+        }, 2000);
+        setTimeout(() => {
+          observer.next('Drugi paketic');
+        }, 4000);
+        setTimeout(() => {
+          // observer.error('Prc');
+          observer.complete();
+        }, 5000);
+        setTimeout(() => {
+          observer.next('3 paketic');
+        }, 6000);
+    } );
+   this.obsPaketici = MyObservable.subscribe(
+      (data: String) => {
+        console.log(data);
+      },
+      (error: String) => {
+        console.log(error);
+      },
+      () => {
+        console.log('Gotovo');
+      }
+      
+    );
+  }
+
+  ngOnDestroy(){
+    this.obsBrojevi.unsubscribe();
+    this.obsPaketici.unsubscribe();
   }
 
 }
